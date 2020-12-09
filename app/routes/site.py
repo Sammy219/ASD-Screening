abe04bfb75bdc4cd98c2d7e519550fc44fa81df9
Fahreza <fahreza_12@apps.ipb.ac.id>
2020-12-10 01:24:38 +0700

    udah bisa nampilih hasil

--- app/routes/site.py
+++ app/routes/site.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import pickle
+
 
 site = Blueprint('site', __name__)
 
@@ -19,8 +20,49 @@ def test():
     return render_template('test.jinja')
 
 
-@site.route('/result', methods=['GET', 'POST'])
+@site.route('/result', methods=['POST'])
 def result():
-    asd_model = pickle.load(open('app/static/asd_model.pkl', 'rb'))
+    data = request.form
+    test_data = preprocess_data(data)
+    print(f'test data = {test_data}')
+    asd_model = pickle.load(open('ASD_Model/pkl_model.pkl', 'rb'))
+    test_result = asd_model.predict([test_data])[0]
+    print(f'ASD RESULT: {test_result}')
 
-    return render_template('result.jinja')
+    return render_template('result.jinja', test_result=test_result)
+
+
+def preprocess_data(data):
+    # order of input: questions 1-10, sex, eth, jnd, fam, ass, 24_36, 0_12
+    test_data = []
+    answers = [data[key] for key in data if key.startswith('q')]
+    print(answers)
+    for i in range(len(answers)):
+        if i == len(answers) - 1:
+            if answers[i] == '1' or answers[i] == '2' or answers[i] == '3':
+                answers[i] = '1'
+            else:
+                answers[i] = '0'
+        else:
+            if answers[i] == '3' or answers[i] == '4' or answers[i] == '5':
+                answers[i] = '1'
+            else:
+                answers[i] = '0'
+    print(answers)
+    test_data = [ans for ans in answers]
+    # sex = male
+    test_data.append('1')
+    # eth = asian
+    test_data.append('6')
+    # jaundice = no
+    test_data.append('0')
+    # family with asd = no
+    test_data.append('0')
+    # assigner = family member
+    test_data.append('0')
+    # within 24_36 = yes
+    test_data.append('1')
+    # within 0_12 = no
+    test_data.append('0')
+
+    return test_data
